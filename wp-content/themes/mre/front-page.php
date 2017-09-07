fb77785e6755d0c34ffd3bd633432fe34325d92c
Jose Ignacio <jpernia@teravisiontech.com>
2017-09-07 19:49:13 -0400

    Made the section before contact

--- wp-content/themes/mre/front-page.php
+++ wp-content/themes/mre/front-page.php
@@ -20,7 +20,90 @@ get_header();
 </section>
 
 <section id="before-contact-us">
+    <div id="countries">
+        <!-- Indicators -->
+        <ul class="indicators">
+            <li data-target="#offices" data-slide-to="0" class="flag">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/ven_flag.svg" />
+            </li>
+            <li data-target="#offices" data-slide-to="1" class="flag active">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/usa_flag.svg" />
+            </li>
+            <li data-target="#offices" data-slide-to="2" class="flag">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/spain_flag.svg" />
+            </li>
+        </ul>
+        <h4>Puedes encontrar Nuestras Oficinas en:</h4>
+    </div>
+    <div id="offices" class="carousel slide" data-ride="carousel" data-interval="false">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item">
+                <div class="offices-detail">
+                    <div class="detail">
+                        <h5>
+                            <span>Sample:</span>
+                        </h5>
+                        <h5>sample sample sample sample</h5>
+                        <h5>sample, sample</h5>
+                        <h5>Sample: +1 111 11.11.11</h5>
+                    </div>
+                    <div class="detail">
+                        <h5>
+                            <span>Sample:</span>
+                        </h5>
+                        <h5>sample sample sample sample</h5>
+                        <h5>sample, sample</h5>
+                        <h5>Sample: +1 111 11.11.11</h5>
+                    </div>
+                </div>
+            </div>
 
+            <div class="item active">
+                <div class="offices-detail">
+                    <div class="offices-detail">
+                        <div class="detail">
+                            <h5>
+                                <span>Orlando:</span>
+                            </h5>
+                            <h5>2295 S. Hiawassee Road, Suite 407E</h5>
+                            <h5>Orlando, Florida</h5>
+                            <h5>Teléfonos: +1 407 255.08.71</h5>
+                        </div>
+                        <div class="detail">
+                            <h5>
+                                <span>Miami:</span>
+                            </h5>
+                            <h5>55 Merrick Way, Suite 214 Coral Gables</h5>
+                            <h5>Miami, Florida</h5>
+                            <h5>Teléfonos: +1 786 376.22.22 / 477.50.91</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="offices-detail">
+                <div class="detail">
+                    <h5>
+                        <span>Sample:</span>
+                    </h5>
+                    <h5>sample sample sample sample</h5>
+                    <h5>sample, sample</h5>
+                    <h5>Sample: +1 111 11.11.11</h5>
+                </div>
+                <div class="detail">
+                    <h5>
+                        <span>Sample:</span>
+                    </h5>
+                    <h5>sample sample sample sample</h5>
+                    <h5>sample, sample</h5>
+                    <h5>Sample: +1 111 11.11.11</h5>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section id="contact-us">
--- wp-content/themes/mre/js/basic.js
+++ wp-content/themes/mre/js/basic.js
@@ -2,12 +2,25 @@
  * Created by mtoledo on 3/8/17.
  */
 
-/* Open when someone clicks on the span element */
+// Open when someone clicks on the span element
 function openNav() {
     document.getElementById('overlay-nav').style.left = '0%';
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
+// Close when someone clicks on the "x" symbol inside the overlay
 function closeNav() {
     document.getElementById('overlay-nav').style.left = '100%';
 }
+
+// jQuery Based Methods
+jQuery(document).ready(function() {
+
+    jQuery('.flag').on('click', function () {
+        let thisFlag = jQuery(this);
+        jQuery('.flag').each(function(){
+            jQuery(this).removeClass('active');
+        });
+        thisFlag.addClass('active');
+    });
+
+});
