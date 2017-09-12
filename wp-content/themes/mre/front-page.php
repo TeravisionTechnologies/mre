b6ae3fa77d0ed3ba315cc697c373a9066b5e5ccf
Jose Ignacio <jpernia@teravisiontech.com>
2017-09-12 18:23:55 -0400

    Modified the plugin for touch Carousels

--- wp-content/themes/mre/front-page.php
+++ wp-content/themes/mre/front-page.php
@@ -177,6 +177,15 @@ $theMeta = get_post_meta($headerPost[0]->ID);
                 </div>
             </div>
         </div>
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#offices" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#offices" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 </section>
 
