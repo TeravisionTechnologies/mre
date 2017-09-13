dc7582ef7cf9ee59497bbd742c76fca9ed25f080
Jose Ignacio <jpernia@teravisiontech.com>
2017-09-13 18:12:36 -0400

    Fixed menu not closing in mobile when options are clicked

--- wp-content/themes/mre/js/basic.js
+++ wp-content/themes/mre/js/basic.js
@@ -2,32 +2,40 @@
  * Created by mtoledo on 3/8/17.
  */
 
-// Open when someone clicks on the span element
+// Open when user clicks on the burger menu
 function openNav() {
     document.getElementById('overlay-nav').style.left = '0%';
 }
 
-// Close when someone clicks on the "x" symbol inside the overlay
+// Close when user clicks on the "x" symbol inside the overlay
 function closeNav() {
     document.getElementById('overlay-nav').style.left = '100%';
 }
 
 jQuery(document).ready(function() {
+
+    // Add active class to the first carousel item of #about-us
     jQuery('#about-us .item:first-child').addClass('active');
+
+    // close overlay in mobile when something is clicked
+    jQuery('.menu-item').on('click', function () {
+        jQuery('#overlay-nav').css({'left':'100%'});
+    });
 
     // Footer button functionality
 
+        var footerButton = jQuery('footer .fa-caret-up');
         // Hide or Show depending if user scrolled after about section
         jQuery(document).on('scroll', function() {
             if ( jQuery('body').scrollTop() <= jQuery('#about-us').position().top ) {
-                jQuery('footer .fa-caret-up').hide();
+                footerButton.hide();
             } else {
-                jQuery('footer .fa-caret-up').show();
+                footerButton.show();
             }
         });
 
         // Scroll slow to the beggining of the page when the button is clicked
-        jQuery('footer .fa-caret-up').on('click', function () {
+        footerButton.on('click', function () {
             jQuery('body').animate({scrollTop:0}, 'slow');
         });
 });
