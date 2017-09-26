b5d4b8923dae76fb37fb33c46fa7ef403a409a1e
Adrián Cárdenas <adriancardenas90@gmail.com>
2017-09-26 14:07:47 -0400

    Refactor footer all resolutions

--- wp-content/themes/mre/footer.php
+++ wp-content/themes/mre/footer.php
@@ -1,46 +1,20 @@
-
-    <footer>
-        <div class="container">
-            <div class="footer-center">
-                <?php
-                $headerPost = get_posts(
-                    array(
-                        'post_type' => 'header_footer'
-                    )
-                );
-                $theMeta = get_post_meta($headerPost[0]->ID);
-                ?>
-                <div class="footer-logo">
-                    <a href="<?php echo esc_url( home_url( '/' ) ) ?>">
-                        <img src="<?php echo $theMeta['_hf_logo'][0] ?>" />
-                    </a>
-                </div>
-                <div class="footer-copy">
-                    <h5>
-                        <?php echo $theMeta['_hf_copy'][0] ?>
-                        <a href="#">Disclaimers</a>
-                    </h5>
-                </div>
-                <div class="footer-icons">
-                    <a href="https://www.facebook.com/MerandRealEstate/">
-                        <i class="fa fa-facebook"></i>
-                    </a>
-                    <a href="https://twitter.com/grupomre">
-                        <i class="fa fa-twitter"></i>
-                    </a>
-                    <a href="https://www.instagram.com/grupomre/">
-                        <i class="fa fa-instagram"></i>
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCj1GOp1JCfAaSmBdQn5uU9w?view_as=subscriber">
-                        <i class="fa fa-youtube-play"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/company/11285534/">
-                        <i class="fa fa-linkedin"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="caretCircle"><span class="fa fa-caret-up"></span></div>
+<?php
+  $footerPost = get_posts(
+    array(
+      'post_type' => 'header_footer'
+    )
+  );
+  $theMeta = get_post_meta($footerPost[0]->ID);
+?>
+    <footer class="container-fluid" id="mre-footer">
+      <div class="mre-footer-elements">
+        <a href="/">
+          <img src="<?php echo $theMeta['_hf_logo'][0] ?>" class="mre-footer-logo"/>
+        </a>
+        <h2 class="mre-footer-copyright"><?php echo $theMeta['_hf_copy'][0] ?></h2>
+        <a href="#">Disclaimers</a>
+      </div>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/go-top-arrow.svg" class="mre-footer-go-top" alt="Go to top button">
     </footer>
   </body>
 </html>
