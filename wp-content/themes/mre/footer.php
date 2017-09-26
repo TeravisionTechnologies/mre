59cd7cf274c09725e30bc949bbfa70a5500b4756
Adrián Cárdenas <adriancardenas90@gmail.com>
2017-09-26 15:32:04 -0400

    Css and JS reorder

--- wp-content/themes/mre/footer.php
+++ wp-content/themes/mre/footer.php
@@ -16,5 +16,14 @@
       </div>
       <img src="<?php echo get_template_directory_uri(); ?>/assets/go-top-arrow.svg" class="mre-footer-go-top" alt="Go to top button">
     </footer>
+
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/swiper.min.js"></script>
+    <script type="text/javascript">
+      var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
+    </script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/basic.js"></script>
+    <?php wp_enqueue_script("jquery"); ?>
   </body>
 </html>
