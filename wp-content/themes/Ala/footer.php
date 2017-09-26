99a1c9b0063112ecf4cf04d19fe49d11d25753ec
mtoledo <mmts048@gmail.com>
2017-09-26 10:52:05 -0400

    Ala19 Footer Added

---
+++ wp-content/themes/Ala/footer.php
@@ -0,0 +1,11 @@
+    <footer class="al-footer-container">
+      <div class="container">
+        <div class="col-xs-12 text-center">
+          <img src="wp-content/themes/Ala/assets/logo-white.png" alt="Logo Ala19 Footer" class="img-responsive center-block">
+          <p class="al-text-white">Todos los derechos reservados, Ala 19 Real Estate C.A.</p>
+          <a href="" class="al-text-white">Disclaimers</a>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ wp-content/themes/Ala/index.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     *  Created by PhpStorm.
+     *  User: mtoledo
+     *  Date: 3/8/17
+     *  Time: 8:19 AM
+     **/
+
+    get_header();
+?>
+
+
+
+<?php get_footer(); ?>
