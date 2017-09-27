1f0b16d8177c3cfc334c3acd95a7f8e542023423
mtoledo <mmts048@gmail.com>
2017-09-26 20:49:19 -0400

    Ala19 Footer: Contact Added

--- wp-content/themes/Ala/includes/post-types/header-footer/meta-boxes.php
+++ wp-content/themes/Ala/includes/post-types/header-footer/meta-boxes.php
@@ -52,4 +52,25 @@
 				'repeatable' => false
 			)
 		);
+
+    // Contact us Background Image
+    $cmb->add_field(
+      array(
+        'name'         => __( 'Background image' ),
+        'desc'         => __( 'Image to display in Contact us section.' ),
+        'id'           => $prefix . 'contact',
+        'type'         => 'file',
+        'preview_size' => array(100,100),
+        'text'         =>
+          array(
+            'add_upload_files_text' => __( 'Add or Upload Images' ), // default: "Add or Upload Files"
+            'file_text'             => __( 'Image:' ), // default: "File:"
+          ),
+        'options' =>
+          array(
+            'url' => false, // Hide the text input for the url
+          ),
+        'repeatable'   => false
+      )
+    );
 	}
