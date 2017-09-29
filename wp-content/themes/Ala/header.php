54f6bcea72301b54cc7b3d1c294c11f800c822c0
Adrián Cárdenas <adriancardenas90@gmail.com>
2017-09-29 18:00:54 -0400

    Issues

--- wp-content/themes/Ala/header.php
+++ wp-content/themes/Ala/header.php
@@ -96,8 +96,8 @@
     </div>
     <section class="col-xs-12 al-projects">
       <div class="container center-block al-project-list">
-        <a class="al-project-list-item"><h2 class="item-text">Proyectos pasados</h2><img class="triangle" src="<?php echo get_template_directory_uri(); ?>/assets/triangle.svg"></a>
-        <a class="al-project-list-item"><h2 class="item-text item-active">Proyectos actuales</h2><img class="triangle" src="<?php echo get_template_directory_uri(); ?>/assets/triangle.svg"></a>
-        <a class="al-project-list-item"><h2 class="item-text">Muy pronto</h2><img class="triangle" src="<?php echo get_template_directory_uri(); ?>/assets/triangle.svg"></a>
+        <span class="al-project-list-item"><h2 class="item-text">Proyectos pasados</h2><img class="triangle" src="<?php echo get_template_directory_uri(); ?>/assets/triangle.svg"></span>
+        <span class="al-project-list-item"><h2 class="item-text item-active">Proyectos actuales</h2><img class="triangle" src="<?php echo get_template_directory_uri(); ?>/assets/triangle.svg"></span>
+        <span class="al-project-list-item"><h2 class="item-text">Muy pronto</h2><img class="triangle" src="<?php echo get_template_directory_uri(); ?>/assets/triangle.svg"></span>
       </div>
     </section>
