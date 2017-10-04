857c532f1cb0ec903eff41c55b48204fe8fd04ee
Adrián Cárdenas <adriancardenas90@gmail.com>
2017-10-04 10:25:31 -0400

    MRE: Footer - All resolutions

--- wp-content/themes/mre/footer.php
+++ wp-content/themes/mre/footer.php
@@ -6,16 +6,16 @@
   );
   $theMeta = get_post_meta($footerPost[0]->ID);
 ?>
-    <!--<footer class="container-fluid" id="mre-footer">
+    <footer id="mre-footer">
       <div class="mre-footer-elements">
         <a href="/">
-          <img src="<?php /*echo $theMeta['_hf_logo'][0] */?>" class="mre-footer-logo"/>
+          <img src="<?php echo $theMeta['_hf_logo'][0] ?>" class="mre-footer-logo"/>
         </a>
-        <h2 class="mre-footer-copyright"><?php /*echo $theMeta['_hf_copy'][0] */?></h2>
+        <h2 class="mre-footer-copyright"><?php echo $theMeta['_hf_copy'][0] ?></h2>
         <a href="#">Disclaimers</a>
       </div>
-      <img src="<?php /*echo get_template_directory_uri(); */?>/assets/go-top-arrow.svg" class="mre-footer-go-top" alt="Go to top button">
-    </footer>-->
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/go-top-arrow.svg" class="mre-footer-go-top" alt="Go to top button">
+    </footer>
 
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.2.1.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/swiper.min.js"></script>
--- wp-content/themes/mre/front-page.php
+++ wp-content/themes/mre/front-page.php
@@ -10,7 +10,91 @@ $headerPost = get_posts(
 );
 $theMeta = get_post_meta($headerPost[0]->ID);
 ?>
-
+<header id="mre-header">
+  <div class="swiper-container swiper-container-menu">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide menu">
+        <ul class="mre-menu">
+          <li class="mre-menu-item"><a href="#">Sobre nosotros</a></li>
+          <li class="mre-menu-item" id="menu-projects"><a href="#">Proyectos</a></li>
+          <li class="mre-menu-item"><a href="#">HR19 realty</a></li>
+          <li class="mre-menu-item"><a href="#">Grupo MRE</a></li>
+          <li class="mre-menu-item" id="menu-contact"><a href="#">Contáctanos</a></li>
+        </ul>
+        <div class="mre-menu-language">
+          <h2 class="mre-menu-language-text">Seleccione su idioma de preferencia:</h2>
+          <img class="mre-menu-language-flag " src="<?php echo get_template_directory_uri(); ?>/assets/spain_flag.svg" alt="Spanish">
+          <img class="mre-menu-language-flag language-flag-active" src="<?php echo get_template_directory_uri(); ?>/assets/usa_flag.svg" alt="English">
+        </div>
+        <div class="mre-menu-social">
+          <ul class="menu-social-icons">
+            <li class="menu-social-icon"><a href="https://www.facebook.com/MerandRealEstate/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li class="menu-social-icon"><a href="https://twitter.com/grupomre"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li class="menu-social-icon"><a href="https://www.instagram.com/grupomre/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            <li class="menu-social-icon"><a href="https://www.youtube.com/channel/UCj1GOp1JCfAaSmBdQn5uU9w?view_as=subscriber"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+            <li class="menu-social-icon"><a href="https://www.linkedin.com/company/11285534/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="swiper-slide content">
+        <div class="menu-button">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
+        </div>
+        <div class="logo-header">
+          <div class="img-div pull-right">
+            <a href=""><img src="wp-content/themes/mre/assets/logo.png" alt="MRE Logo"></a>
+          </div>
+        </div>
+        <div class="social-header pull-right">
+          <ul class="social-icons">
+            <li class="social-icon"><a href="https://www.facebook.com/MerandRealEstate/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li class="social-icon"><a href="https://twitter.com/grupomre"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li class="social-icon"><a href="https://www.instagram.com/grupomre/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            <li class="social-icon"><a href="https://www.youtube.com/channel/UCj1GOp1JCfAaSmBdQn5uU9w?view_as=subscriber"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+            <li class="social-icon"><a href="https://www.linkedin.com/company/11285534/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+        <div class="swiper-container swiper-container-hero">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/hero-1.jpg');">
+              <div class="slide-overlay"></div>
+              <div class="slide-text">
+                <h2>Comprometidos con tu</h2>
+                <h3>TU FUTURO</h3>
+              </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/hero-2.jpg');">
+              <div class="slide-overlay"></div>
+              <div class="slide-text">
+                <h2>Especialistas en</h2>
+                <h3>LUXURY RENTAL</h3>
+              </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/hero-3.jpg');">
+              <div class="slide-overlay"></div>
+              <div class="slide-text">
+                <h2>Innovación y diseño</h2>
+                <h3>VANGUARDISTA</h3>
+              </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/hero-4.jpg');">
+              <div class="slide-overlay"></div>
+              <div class="slide-text">
+                <h2>Estamos contigo en</h2>
+                <h3>TODO MOMENTO</h3>
+              </div>
+            </div>
+          </div>
+          <i class="fa fa-chevron-circle-left swiper-button-prev" aria-hidden="true"></i>
+          <i class="fa fa-chevron-circle-right swiper-button-next" aria-hidden="true"></i>
+        </div>
+        <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/hero-arrow.svg" class="hero-button"></a>
+      </div>
+    </div>
+  </div>
+</header>
 <section id="mre-about-us" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/about-us-bg.jpg');" alt="About Us Background">
   <h3 class="about-us-title no-margin">Sobre Nosotros</h3>
   <h2 class="about-us-sub-first no-margin">Bienvenido a</h2>
