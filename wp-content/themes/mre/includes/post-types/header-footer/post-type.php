6daaa06c3d2e540df229b979c878217f6baf7996
Unknown <adriancardenas90@gmail.com>
2017-10-09 17:48:31 -0400

    MRE: CPT Changed

--- wp-content/themes/mre/includes/post-types/header-footer/post-type.php
+++ wp-content/themes/mre/includes/post-types/header-footer/post-type.php
@@ -10,14 +10,14 @@
 		register_post_type( 'header_footer',
 			array(
 				'labels' => array(
-					'name'          => __( 'Header & Footer Area' ),
-					'singular_name' => __( 'Header & Footer Area' ),
-					'add_new'       => __( 'Add a New Header & Footer Area' ),
-					'edit_item'     => __( 'Edit Header & Footer Area' ),
-					'new_item'      => __( 'New Header & Footer Area' ),
-					'view_item'     => __( 'View Header & Footer Area' ),
-					'view_items'    => __( 'View Header & Footer Area' ),
-					'all_items'     => __( 'Header & Footer Area' )
+					'name'          => __( 'Header, Footer & Homepage Area' ),
+					'singular_name' => __( 'Header, Footer & Homepage Area' ),
+					'add_new'       => __( 'Add a New Header, Footer & Homepage Area' ),
+					'edit_item'     => __( 'Edit Header, Footer & Homepage' ),
+					'new_item'      => __( 'New Header, Footer & Homepage' ),
+					'view_item'     => __( 'View Header, Footer & Homepage' ),
+					'view_items'    => __( 'View Header, Footer & Homepage' ),
+					'all_items'     => __( 'Header, Footer & Homepage' )
 				),
 				'public' => true
 			)
