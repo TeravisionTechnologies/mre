ef1495dc6950f977f471b6ef039fa117115dc332
Unknown <adriancardenas90@gmail.com>
2017-10-19 16:38:41 -0400

    HR19: Fonts, colors, functions

--- wp-content/themes/hr19/index.php
+++ wp-content/themes/hr19/index.php
@@ -1,7 +1,16 @@
 <?php
     get_header();
 ?>
-
-<div class="text-center"> Hello World! </div>
+  <section class="col-xs-12" style="height: 200px; background-color: red;">
+  </section>
+  <section class="col-xs-12 hr-partners-section text-center">
+    <div class="container">
+      <p class="hr-partners-title">La nueva forma de <span>invertir en propiedades</span></p>
+    </div>
+    <div class="hr-partners-images">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/ala19.svg" alt="Logo ALA19" class="partners-images-one"/>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/mre.svg" alt="Logo MRE RealEstate" class="partners-images-two"/>
+    </div>
+  </section>
 
 <?php get_footer(); ?>
