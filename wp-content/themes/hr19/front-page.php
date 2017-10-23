5013a6990087b1ca934806143d4f0e3b378814e4
JRIVERO-PC\JORGE RIVERO <jorgeriverorivera@gmail.com>
2017-10-23 09:33:51 -0400

    HR19: Pagination

--- wp-content/themes/hr19/front-page.php
+++ wp-content/themes/hr19/front-page.php
@@ -131,6 +131,29 @@
                 </div>
             <?php } ?>
         </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <nav>
+                    <ul class="pagination">
+                        <li>
+                            <a href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li><a href="#" class="active">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </div>
   <section class="col-xs-12 hr-partners-section text-center">
     <div class="container">
