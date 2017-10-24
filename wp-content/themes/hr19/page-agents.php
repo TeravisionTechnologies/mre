4194a8b9da2ef791b72b1db98e16fa91b633746c
JRIVERO-PC\JORGE RIVERO <jorgeriverorivera@gmail.com>
2017-10-24 10:19:48 -0400

    HR19: Page agents - Hero

--- wp-content/themes/hr19/page-agents.php
+++ wp-content/themes/hr19/page-agents.php
@@ -1,34 +1,18 @@
 <?php
-    /*
-    Template Name: Agents
-    */
-    get_header();
-    //$phone = get_post_meta( get_the_ID(), '_ag_phone', true);
-    //$email = get_post_meta( get_the_ID(), '_ag_mail', true);
+/*
+Template Name: Agents
+*/
+get_header();
+//$phone = get_post_meta( get_the_ID(), '_ag_phone', true);
+//$email = get_post_meta( get_the_ID(), '_ag_mail', true);
 ?>
 
-<section class="agent-hero">
-    <h1><?php the_title(); ?></h1>
-</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <section class="agent-hero" style="<?php if ($thumbnail_id = get_post_thumbnail_id()) {
+        if ($image_src = wp_get_attachment_image_src($thumbnail_id, 'full')) printf('background: rgba(0, 0, 0, 0.4) url(%s)"', $image_src[0]);
+    } ?>">
+        <h1><?php the_title(); ?></h1>
+        <div class="mask"></div>
+    </section>
 
 
 <?php get_footer();
