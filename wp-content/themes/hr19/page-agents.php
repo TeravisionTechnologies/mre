c1cdd23f795681744bfa3c24dbd9d5c452259cdd
JRIVERO-PC\JORGE RIVERO <jorgeriverorivera@gmail.com>
2017-10-24 09:32:10 -0400

    HR19: Change single-agent.php for page-agents.php

---
+++ wp-content/themes/hr19/page-agents.php
@@ -0,0 +1,30 @@
+<?php
+    /*
+    Template Name: Agents
+    */
+    get_header();
+    //$phone = get_post_meta( get_the_ID(), '_ag_phone', true);
+    //$email = get_post_meta( get_the_ID(), '_ag_mail', true);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer();
