02775fc1a70e5d5802eed92a5ea1ac6358db9280
Unknown <adriancardenas90@gmail.com>
2017-10-24 15:54:37 -0400

    HR19: Agents page

--- wp-content/themes/hr19/page-agents.php
+++ wp-content/themes/hr19/page-agents.php
@@ -26,6 +26,40 @@ get_header();
     <div class="agent-bio">
       <p class="agent-bio-text">Mr. Tomas Hoffman is a real estate broker based in Miami, Florida. As a Venezuelan-American who moved to Miami in the early eighties he has seen the city steadily evolve into the metropolis it is today, and has proudly been directly involved in its development. Over his 25-year career Tomas has brokered over $300 million worth of commercial and residential real estate throughout Miami-Dade County. His areas of expertise include investment properties, lending, land development and pre construction. Tomas is currently involved in several ongoing projects in Brickell, Miami Beach, and Sunny Isles</p>
     </div>
+    <div class="agent-properties">
+      <h2 class="properties-number">Propiedades asignadas (9)<i class="fa fa-caret-down" aria-hidden="true"></i></h2>
+      <div class="properties-list">
+        <div class="col-xs-12 col-sm-4 no-padding property">
+          <div class="property-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/rentalone-background.jpg')"></div>
+          <div class="property-info">
+            <h2 class="info-price">$150,000</h2>
+            <h3 class="info-features">Unifamiliar · 2 Habitaciones · 2 Baños</h3>
+            <h3 class="info-address">Florissant, MO 63031</h3>
+            <h3 class="info-mls">MLS: 1258590</h3>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-4 no-padding property">
+          <div class="property-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/rentalone-background.jpg')"></div>
+          <div class="property-info">
+            <h2 class="info-price">$150,000</h2>
+            <h3 class="info-features">Unifamiliar · 2 Habitaciones · 2 Baños</h3>
+            <h3 class="info-address">Florissant, MO 63031</h3>
+            <h3 class="info-mls">MLS: 1258590</h3>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-4 no-padding property">
+          <div class="property-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/rentalone-background.jpg')"></div>
+          <div class="property-info">
+            <h2 class="info-price">$150,000</h2>
+            <h3 class="info-features">Unifamiliar · 2 Habitaciones · 2 Baños</h3>
+            <h3 class="info-address">Florissant, MO 63031</h3>
+            <h3 class="info-mls">MLS: 1258590</h3>
+          </div>
+        </div>
+
+        <button class="more-properties">Ver más propiedades</button>
+      </div>
+    </div>
 
 
 
