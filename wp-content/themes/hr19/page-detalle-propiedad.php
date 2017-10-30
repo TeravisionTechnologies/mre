8a53af5dae46738d53b59545e74c460ef6bcfcba
JRIVERO-PC\JORGE RIVERO <jorgeriverorivera@gmail.com>
2017-10-30 17:37:09 -0400

    HR19: Property Detail

---
+++ wp-content/themes/hr19/page-detalle-propiedad.php
@@ -0,0 +1,89 @@
+<?php
+get_header();
+?>
+
+<div class="breadcrumb-info">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-7 col-md-6">
+                <div class="breadcrumbs"><i class="fa fa-chevron-left" aria-hidden="true"></i> Miami, FL  >  153 Jost Manor Dr Florissant, MO 63034</div>
+            </div>
+            <div class="col-xs-12 col-sm-5 col-md-6 text-right">
+                <div class="published">Publicada hace: 59 días</div>
+                <div class="status">Estatus: <span>Activa</span></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="property-carousel" style="height: 300px">
+
+</div>
+
+<div class="property-info-heading">
+
+</div>
+
+<div class="property-features">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse1"><span class="number">1</span>Collapsible Group 1</a>
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                            </h4>
+                        </div>
+                        <div id="collapse1" class="panel-collapse collapse in">
+                            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                                    Collapsible Group 2</a>
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                            </h4>
+                        </div>
+                        <div id="collapse2" class="panel-collapse collapse">
+                            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                                    Collapsible Group 3</a>
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                            </h4>
+                        </div>
+                        <div id="collapse3" class="panel-collapse collapse">
+                            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="property-similar">
+
+</div>
+
+<?php get_footer(); ?>
