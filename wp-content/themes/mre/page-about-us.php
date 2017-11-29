55aa59c95ebac5d0f30499831c07baf65bb8ffc9
JRIVERO-PC\JORGE RIVERO <jorgeriverorivera@gmail.com>
2017-11-29 19:45:18 -0400

    MRE: Banner services

--- wp-content/themes/mre/page-about-us.php
+++ wp-content/themes/mre/page-about-us.php
@@ -2,102 +2,120 @@
 /*
 Template Name: About Us
 */
-
-$about_query = get_posts(
-  array(
-    'post_type' => 'about_us'
-  )
-);
-$headerPost = get_posts(
+get_header();
+$about_query  = get_posts(
 	array(
-		'post_type' => 'header_footer',
+		'post_type' => 'about_us'
+	)
+);
+$headerPost   = get_posts(
+	array(
+		'post_type'   => 'header_footer',
 		'numberposts' => 1
 	)
 );
-$theMeta = get_post_meta($headerPost[0]->ID);
-$hero = get_post_meta( $about_query[0]->ID, '_au_hero', true );
-$main_text = get_post_meta( $about_query[0]->ID, '_au_main', true );
-$values = get_post_meta( $about_query[0]->ID, '_au_values', true );
-$contact = get_post_meta( $headerPost[0]->ID, '_hf_contact_form', true );
-get_header();
+$headerPost   = get_posts(
+	array(
+		'post_type'   => 'header_footer',
+		'numberposts' => 1
+	)
+);
+$theMeta      = get_post_meta( $headerPost[0]->ID );
+$hero         = get_post_meta( $about_query[0]->ID, '_au_hero', true );
+$main_text    = get_post_meta( $about_query[0]->ID, '_au_main', true );
+$values       = get_post_meta( $about_query[0]->ID, '_au_values', true );
+$contact      = get_post_meta( $headerPost[0]->ID, '_hf_contact_form', true );
+$partnerLeft  = get_post_meta( $headerPost[0]->ID, '_hf_partner_left', true );
+$partnerRight = get_post_meta( $headerPost[0]->ID, '_hf_partner_right', true );
 ?>
 
-<section  id="about-hero-section" style="background-image: url(<?php echo $hero[0]['_au_hero_background']; ?>)">
-  <div class="about-hero-overlay">
-    <?php echo $hero[0]['_au_hero_text']; ?>
-  </div>
-</section>
-<section id="about-main-text">
-  <?php echo $main_text[0]['_au_main_text']; ?>
-</section>
-<section id="about-secondary-text">
-  <div class="vision col-xs-12 col-sm-6">
-    <h2>Visión</h2>
-    <p><?php echo $main_text[0]['_au_vision_text']; ?></p>
-  </div>
-  <div class="mission col-xs-12 col-sm-6">
-    <h2>Misión</h2>
-    <p><?php echo $main_text[0]['_au_mission_text']; ?></p>
-  </div>
-  <div id="about-values" class="col-xs-12 no-padding">
-    <h2 class="values-title">Valores</h2>
-    <?php foreach ($values as $value) { ?>
-      <div class="col-xs-12 col-sm-6 value-block">
-        <div class="value-image">
-          <img class="img-responsive" src="<?php echo $value['_au_value_image'];?>" alt="<?php echo $value['_au_value_title']; ?>">
+    <section id="about-hero-section" style="background-image: url(<?php echo $hero[0]['_au_hero_background']; ?>)">
+        <div class="about-hero-overlay">
+			<?php echo $hero[0]['_au_hero_text']; ?>
         </div>
-        <h2><?php echo $value['_au_value_title']; ?></h2>
-        <p><?php echo $value['_au_value_text']; ?></p>
-      </div>
-    <?php } ?>
-  </div>
-  <div class="our-team">
-    <h2>Nuestro equipo</h2>
-    <p><?php echo $main_text[0]['_au_team_text']; ?></p>
-  </div>
-  <div class="our-properties">
-    <h2>Nuestras propiedades</h2>
-    <p><?php echo $main_text[0]['_au_properties_text']; ?></p>
-  </div>
-</section>
-<?php
-  $headerPost = get_posts(
-    array(
-      'post_type' => 'header_footer',
-      'numberposts' => 1
-    )
-  );
-  $partnerLeft = get_post_meta( $headerPost[0]->ID, '_hf_partner_left', true );
-  $partnerRight = get_post_meta( $headerPost[0]->ID, '_hf_partner_right', true );
-?>
-  <section class="col-xs-12 hr-partners-section text-center">
-    <div class="container">
-      <p class="hr-partners-title">La nueva forma de <strong>invertir en propiedades</strong></p>
-    </div>
-    <div class="hr-partners-images">
-      <a href="<?php echo $partnerLeft[0]['_hf_partner_link_left']; ?>" target="_blank"><img src="<?php echo $partnerLeft[0]["_hf_partner_left_logo"]; ?>" alt="Logo ALA19" class="partners-images-one"/></a>
-      <a href="<?php echo $partnerRight[0]['_hf_partner_link_right']; ?>" target="_blank"><img src="<?php echo $partnerRight[0]["_hf_partner_right_logo"]; ?>" alt="Logo MRE RealEstate" class="partners-images-two"/></a>
-    </div>
-  </section>
-  <section id="contact-us" class="col-xs-12 al-contact-div no-padding" style="background-image: url('<?php if(isset($contact[0]["_hf_contact_background"])) { echo $contact[0]["_hf_contact_background"]; }?>')">
-    <div class="overlay"></div>
-    <div class="container-mre center-block">
-      <div class="row">
-        <p class="col-xs-12 text-center al-contact-text"><?php if(isset($contact[0]['_hf_contact_first'])) { echo $contact[0]['_hf_contact_first']; }?></p>
-        <p class="col-xs-12 text-center al-contact-text-bold"><?php if(isset($contact[0]['_hf_contact_second'])) { echo $contact[0]['_hf_contact_second']; }?></p>
-        <div class="col-xs-12 col-md-4 no-padding">
-          <div class="al-phone-box text-center center-block">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/smartphone.svg" alt="Llamanos Ala19">
-            <p><?php if(isset($contact[0]['_hf_contact_text'])) { echo $contact[0]['_hf_contact_text']; }?></p>
-            <?php if(isset($contact[0]['_hf_contact_phone'])) { ?>
-              <a href="tel:<?php echo str_replace(array(".", " ", "-", "/"), "", $contact[0]['_hf_contact_phone']); ?>" class="al-phone-num"><?php echo $contact[0]['_hf_contact_phone']; ?></a>
-            <?php } ?>
-          </div>
+    </section>
+    <section id="about-main-text">
+		<?php echo $main_text[0]['_au_main_text']; ?>
+    </section>
+    <section id="about-secondary-text">
+        <div class="vision col-xs-12 col-sm-6">
+            <h2>Visión</h2>
+            <p><?php echo $main_text[0]['_au_vision_text']; ?></p>
         </div>
-        <div class="col-xs-12 col-md-8 al-contact-form-div no-padding">
-          <?php echo do_shortcode( '[contact-form-7 id="4" title="Home - Contact form"]' ); ?>
+        <div class="mission col-xs-12 col-sm-6">
+            <h2>Misión</h2>
+            <p><?php echo $main_text[0]['_au_mission_text']; ?></p>
         </div>
-      </div>
-    </div>
-  </section>
+        <div id="about-values" class="col-xs-12 no-padding">
+            <h2 class="values-title">Valores</h2>
+			<?php foreach ( $values as $value ) { ?>
+                <div class="col-xs-12 col-sm-6 value-block">
+                    <div class="value-image">
+                        <img class="img-responsive" src="<?php echo $value['_au_value_image']; ?>"
+                             alt="<?php echo $value['_au_value_title']; ?>">
+                    </div>
+                    <h2><?php echo $value['_au_value_title']; ?></h2>
+                    <p><?php echo $value['_au_value_text']; ?></p>
+                </div>
+			<?php } ?>
+        </div>
+        <div class="our-team">
+            <h2>Nuestro equipo</h2>
+            <p><?php echo $main_text[0]['_au_team_text']; ?></p>
+        </div>
+        <div class="our-properties">
+            <h2>Nuestras propiedades</h2>
+            <p><?php echo $main_text[0]['_au_properties_text']; ?></p>
+        </div>
+    </section>
+    <section class="col-xs-12 hr-partners-section text-center">
+        <div class="container">
+            <p class="hr-partners-title">La nueva forma de <strong>invertir en propiedades</strong></p>
+        </div>
+        <div class="hr-partners-images">
+            <a href="<?php echo $partnerLeft[0]['_hf_partner_link_left']; ?>" target="_blank"><img
+                        src="<?php echo $partnerLeft[0]["_hf_partner_left_logo"]; ?>" alt="Logo ALA19"
+                        class="partners-images-one"/></a>
+            <a href="<?php echo $partnerRight[0]['_hf_partner_link_right']; ?>" target="_blank"><img
+                        src="<?php echo $partnerRight[0]["_hf_partner_right_logo"]; ?>" alt="Logo MRE RealEstate"
+                        class="partners-images-two"/></a>
+        </div>
+    </section>
+    <section id="contact-us" class="col-xs-12 al-contact-div no-padding"
+             style="background-image: url('<?php if ( isset( $contact[0]["_hf_contact_background"] ) ) {
+		         echo $contact[0]["_hf_contact_background"];
+	         } ?>')">
+        <div class="overlay"></div>
+        <div class="container-mre center-block">
+            <div class="row">
+                <p class="col-xs-12 text-center al-contact-text"><?php if ( isset( $contact[0]['_hf_contact_first'] ) ) {
+						echo $contact[0]['_hf_contact_first'];
+					} ?></p>
+                <p class="col-xs-12 text-center al-contact-text-bold"><?php if ( isset( $contact[0]['_hf_contact_second'] ) ) {
+						echo $contact[0]['_hf_contact_second'];
+					} ?></p>
+                <div class="col-xs-12 col-md-4 no-padding">
+                    <div class="al-phone-box text-center center-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/smartphone.svg"
+                             alt="Llamanos Ala19">
+                        <p><?php if ( isset( $contact[0]['_hf_contact_text'] ) ) {
+								echo $contact[0]['_hf_contact_text'];
+							} ?></p>
+						<?php if ( isset( $contact[0]['_hf_contact_phone'] ) ) { ?>
+                            <a href="tel:<?php echo str_replace( array(
+								".",
+								" ",
+								"-",
+								"/"
+							), "", $contact[0]['_hf_contact_phone'] ); ?>"
+                               class="al-phone-num"><?php echo $contact[0]['_hf_contact_phone']; ?></a>
+						<?php } ?>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-8 al-contact-form-div no-padding">
+					<?php echo do_shortcode( '[contact-form-7 id="4" title="Home - Contact form"]' ); ?>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php get_footer(); ?>
--- wp-content/themes/mre/page-services.php
+++ wp-content/themes/mre/page-services.php
@@ -3,82 +3,107 @@
 Template Name: Services
 */
 
-$serv_query = get_posts(
+get_header();
+$serv_query   = get_posts(
 	array(
 		'post_type' => 'services'
 	)
 );
-$headerPost = get_posts(
+$headerPost   = get_posts(
 	array(
-		'post_type' => 'header_footer',
+		'post_type'   => 'header_footer',
 		'numberposts' => 1
 	)
 );
-$hero = get_post_meta( $serv_query[0]->ID, '_sv_hero', true );
-$main_text = get_post_meta( $serv_query[0]->ID, '_sv_main', true );
-$services = get_post_meta( $serv_query[0]->ID, '_sv_services', true );
-
-$contact = get_post_meta( $headerPost[0]->ID, '_hf_contact_form', true );
-
-get_header();
+$headerPost   = get_posts(
+	array(
+		'post_type'   => 'header_footer',
+		'numberposts' => 1
+	)
+);
+$hero         = get_post_meta( $serv_query[0]->ID, '_sv_hero', true );
+$main_text    = get_post_meta( $serv_query[0]->ID, '_sv_main', true );
+$services     = get_post_meta( $serv_query[0]->ID, '_sv_services', true );
+$contact      = get_post_meta( $headerPost[0]->ID, '_hf_contact_form', true );
+$partnerLeft  = get_post_meta( $headerPost[0]->ID, '_hf_partner_left', true );
+$partnerRight = get_post_meta( $headerPost[0]->ID, '_hf_partner_right', true );
 ?>
-	<section id="about-hero-section" style="background-image: url(<?php echo $hero[0]['_sv_hero_background']; ?>)">
-		<div class="about-hero-overlay">
+    <section id="about-hero-section" style="background-image: url(<?php echo $hero[0]['_sv_hero_background']; ?>)">
+        <div class="about-hero-overlay">
 			<?php echo $hero[0]['_sv_hero_text']; ?>
-		</div>
-	</section>
-	<section id="about-main-text">
+        </div>
+    </section>
+    <section id="about-main-text">
 		<?php echo $main_text[0]['_sv_main_text']; ?>
         <div class="servs">
-            <?php echo $main_text[0]['_sv_services_text']; ?>
+			<?php echo $main_text[0]['_sv_services_text']; ?>
         </div>
         <div id="services-icon" class="row">
-			<?php foreach ($services as $service) { ?>
+			<?php foreach ( $services as $service ) { ?>
                 <div class="col-xs-6 col-sm-3 col-md-3 text-center">
-                    <div class="srv-icon"><img class="img-responsive" src="<?php echo $service['_sv_serv_image'];?>"></div>
+                    <div class="srv-icon"><img class="img-responsive" src="<?php echo $service['_sv_serv_image']; ?>">
+                    </div>
                     <div class="srv-tlt"><?php echo $service['_sv_serv_title']; ?></div>
                 </div>
 			<?php } ?>
         </div>
-	</section>
-<?php
-$headerPost = get_posts(
-	array(
-		'post_type' => 'header_footer',
-		'numberposts' => 1
-	)
-);
-$partnerLeft = get_post_meta( $headerPost[0]->ID, '_hf_partner_left', true );
-$partnerRight = get_post_meta( $headerPost[0]->ID, '_hf_partner_right', true );
-?>
-	<section class="col-xs-12 hr-partners-section text-center">
-		<div class="container">
-			<p class="hr-partners-title">La nueva forma de <strong>invertir en propiedades</strong></p>
-		</div>
-		<div class="hr-partners-images">
-			<a href="<?php echo $partnerLeft[0]['_hf_partner_link_left']; ?>" target="_blank"><img src="<?php echo $partnerLeft[0]["_hf_partner_left_logo"]; ?>" alt="Logo ALA19" class="partners-images-one"/></a>
-			<a href="<?php echo $partnerRight[0]['_hf_partner_link_right']; ?>" target="_blank"><img src="<?php echo $partnerRight[0]["_hf_partner_right_logo"]; ?>" alt="Logo MRE RealEstate" class="partners-images-two"/></a>
-		</div>
-	</section>
-	<section id="contact-us" class="col-xs-12 al-contact-div no-padding" style="background-image: url('<?php if(isset($contact[0]["_hf_contact_background"])) { echo $contact[0]["_hf_contact_background"]; }?>')">
-		<div class="overlay"></div>
-		<div class="container-mre center-block">
-			<div class="row">
-				<p class="col-xs-12 text-center al-contact-text"><?php if(isset($contact[0]['_hf_contact_first'])) { echo $contact[0]['_hf_contact_first']; }?></p>
-				<p class="col-xs-12 text-center al-contact-text-bold"><?php if(isset($contact[0]['_hf_contact_second'])) { echo $contact[0]['_hf_contact_second']; }?></p>
-				<div class="col-xs-12 col-md-4 no-padding">
-					<div class="al-phone-box text-center center-block">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/smartphone.svg" alt="Llamanos Ala19">
-						<p><?php if(isset($contact[0]['_hf_contact_text'])) { echo $contact[0]['_hf_contact_text']; }?></p>
-						<?php if(isset($contact[0]['_hf_contact_phone'])) { ?>
-							<a href="tel:<?php echo str_replace(array(".", " ", "-", "/"), "", $contact[0]['_hf_contact_phone']); ?>" class="al-phone-num"><?php echo $contact[0]['_hf_contact_phone']; ?></a>
+    </section>
+    <section id="our-ebooks" class="col-xs-12 text-center"
+             style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/imageservices.png)">
+        <div class="mask">
+            <h3><?php _e( 'Disfruta de tu tiempo', 'mre' ) ?></h3>
+            <p><?php _e( 'mientras nosotros gestionamos tu propiedad', 'mre' ) ?></p>
+            <a href="<?php echo ( $lang == 'es_ES' ? home_url('nuestros-servicios') : home_url('en/our-services') ) ?>" class="btn"><?php _e( 'Ve nuestros servicios', 'mre' ) ?></a>
+        </div>
+    </section>
+    <section class="col-xs-12 hr-partners-section text-center">
+        <div class="container">
+            <p class="hr-partners-title">La nueva forma de <strong>invertir en propiedades</strong></p>
+        </div>
+        <div class="hr-partners-images">
+            <a href="<?php echo $partnerLeft[0]['_hf_partner_link_left']; ?>" target="_blank"><img
+                        src="<?php echo $partnerLeft[0]["_hf_partner_left_logo"]; ?>" alt="Logo ALA19"
+                        class="partners-images-one"/></a>
+            <a href="<?php echo $partnerRight[0]['_hf_partner_link_right']; ?>" target="_blank"><img
+                        src="<?php echo $partnerRight[0]["_hf_partner_right_logo"]; ?>" alt="Logo MRE RealEstate"
+                        class="partners-images-two"/></a>
+        </div>
+    </section>
+    <section id="contact-us" class="col-xs-12 al-contact-div no-padding"
+             style="background-image: url('<?php if ( isset( $contact[0]["_hf_contact_background"] ) ) {
+		         echo $contact[0]["_hf_contact_background"];
+	         } ?>')">
+        <div class="overlay"></div>
+        <div class="container-mre center-block">
+            <div class="row">
+                <p class="col-xs-12 text-center al-contact-text"><?php if ( isset( $contact[0]['_hf_contact_first'] ) ) {
+						echo $contact[0]['_hf_contact_first'];
+					} ?></p>
+                <p class="col-xs-12 text-center al-contact-text-bold"><?php if ( isset( $contact[0]['_hf_contact_second'] ) ) {
+						echo $contact[0]['_hf_contact_second'];
+					} ?></p>
+                <div class="col-xs-12 col-md-4 no-padding">
+                    <div class="al-phone-box text-center center-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/smartphone.svg"
+                             alt="Llamanos">
+                        <p><?php if ( isset( $contact[0]['_hf_contact_text'] ) ) {
+								echo $contact[0]['_hf_contact_text'];
+							} ?></p>
+						<?php if ( isset( $contact[0]['_hf_contact_phone'] ) ) { ?>
+                            <a href="tel:<?php echo str_replace( array(
+								".",
+								" ",
+								"-",
+								"/"
+							), "", $contact[0]['_hf_contact_phone'] ); ?>"
+                               class="al-phone-num"><?php echo $contact[0]['_hf_contact_phone']; ?></a>
 						<?php } ?>
-					</div>
-				</div>
-				<div class="col-xs-12 col-md-8 al-contact-form-div no-padding">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-8 al-contact-form-div no-padding">
 					<?php echo do_shortcode( '[contact-form-7 id="4" title="Home - Contact form"]' ); ?>
-				</div>
-			</div>
-		</div>
-	</section>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php get_footer(); ?>
