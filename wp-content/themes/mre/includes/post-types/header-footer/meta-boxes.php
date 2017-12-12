dbd6cde3782e2ab0ee62255b0171d11d30a602ee
Jorge-PC\Jorge <jorgeriverorivera@gmail.com>
2017-12-11 21:48:03 -0430

    Set services section bg image

--- wp-content/themes/mre/includes/post-types/header-footer/meta-boxes.php
+++ wp-content/themes/mre/includes/post-types/header-footer/meta-boxes.php
@@ -648,5 +648,27 @@ function header_footer_metaboxes() {
 			'repeatable'   => false
 		)
 	);
+
+
+	// Our services background
+	$cmb->add_field(
+		array(
+			'name'         => __( 'Our services background' ),
+			'id'           => $prefix . 'srvsbg',
+			'type'         => 'file',
+			'preview_size' => array( 100, 100 ),
+			'text'         =>
+				array(
+					'add_upload_files_text' => __( 'Add or Upload Images' ), // default: "Add or Upload Files"
+					'file_text'             => __( 'Image:' ), // default: "File:"
+				),
+			'options'      =>
+				array(
+					'url' => false, // Hide the text input for the url
+				),
+			'repeatable'   => false
+		)
+	);
+
 }
 
