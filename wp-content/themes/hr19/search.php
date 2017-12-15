8af20b9e86b831cff8e5504ac6dd095f96957adc
JRIVERO-PC\JORGE RIVERO <jorgeriverorivera@gmail.com>
2017-12-15 17:04:50 -0400

    Search fix

--- wp-content/themes/hr19/search.php
+++ wp-content/themes/hr19/search.php
@@ -6,7 +6,7 @@ if(function_exists('pll_current_language')){
 	if($current_language != $default_language){
 		$language_subdir = $current_language.'/';
 	} else {
-		$language_subdir = '';
+		$language_subdir = '/es';
 	}
 }
 $s = get_query_var('s');
