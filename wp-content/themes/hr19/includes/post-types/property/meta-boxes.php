2b9684c17373b1bd849e4939435b8745e234ae12
JRIVERO-PC\JORGE RIVERO <jorgeriverorivera@gmail.com>
2018-02-21 17:30:48 -0400

    MRE: Presale backend

--- wp-content/themes/hr19/includes/post-types/property/meta-boxes.php
+++ wp-content/themes/hr19/includes/post-types/property/meta-boxes.php
@@ -846,4 +846,242 @@ function property_metaboxes() {
 		'type' => 'file_list',
 	) );
 
+	$cmb_property->add_field( array(
+		'name' => '<div style="background: #23282d;color: #fff;padding: 10px">PROPIEDADES EN PREVENTA - HR19</div>',
+		'desc' => 'Los campos a continuacion pertenecen a las propiedades que seran mostradas con la plantilla de ALA19',
+		'type' => 'title',
+		'id'   => 'preventa_title'
+	) );
+
+	$cmb_property->add_field( array(
+		'name'         => __( 'Property Image' ),
+		'desc'         => 'Upload an image',
+		'id'           => $prefix . 'images',
+		'type'         => 'file',
+		// Optional:
+		'options'      => array(
+			'url' => false, // Hide the text input for the url
+		),
+		'text'         => array(
+			'add_upload_file_text' => 'Add image' // Change upload button text. Default: "Add or Upload File"
+		),
+		'preview_size' => 'small', // Image size to use when previewing in the admin.
+	) );
+
+
+	// Property Location
+	$cmb_property->add_field(
+		array(
+			'name'       => __( 'Property Location' ),
+			'desc'       => __( 'The location of the current property' ),
+			'id'         => $prefix . 'price',
+			'type'       => 'text',
+			'repeatable' => false
+		)
+	);
+
+	// Property Address
+	$cmb_property->add_field(
+		array(
+			'name'       => __( 'Property Address' ),
+			'desc'       => __( 'The address of the current property' ),
+			'id'         => $prefix . 'address',
+			'type'       => 'text',
+			'repeatable' => false
+		)
+	);
+
+	// Interior Details Text
+	$cmb_property->add_field(
+		array(
+			'name'    => __( 'Interior Details' ),
+			'id'      => $prefix . 'intdetails',
+			'type'    => 'wysiwyg',
+			'options' => array(),
+		)
+	);
+
+	// Interior Details Gallery
+	$cmb_property->add_field(
+		array(
+			'name'         => __( 'Interior Details Images' ),
+			'id'           => $prefix . 'intimages',
+			'type'         => 'file_list',
+			'preview_size' => array( 100, 100 ),
+			'text'         =>
+				array(
+					'add_upload_files_text' => __( 'Add or Upload Images' ), // default: "Add or Upload Files"
+					'file_text'             => __( 'Image:' ), // default: "File:"
+				),
+			'repeatable'   => false
+		)
+	);
+
+	// Amenities
+	$cmb_property->add_field(
+		array(
+			'name'    => 'Amenities',
+			'id'      => $prefix . 'amen',
+			'type'    => 'wysiwyg',
+			'options' => array(),
+		)
+	);
+
+	// Amenities Gallery
+	$cmb_property->add_field(
+		array(
+			'name'         => __( 'Amenities Images' ),
+			'id'           => $prefix . 'amengallery',
+			'type'         => 'file_list',
+			'preview_size' => array( 100, 100 ),
+			'text'         =>
+				array(
+					'add_upload_files_text' => __( 'Add or Upload Images' ), // default: "Add or Upload Files"
+					'file_text'             => __( 'Image:' ), // default: "File:"
+				),
+			'repeatable'   => false
+		)
+	);
+
+	// Plains Carousel
+	$cmb_property->add_field(
+		array(
+			'name'         => __( 'Plains Images / Videos' ),
+			'id'           => $prefix . 'plainsgallery',
+			'type'         => 'file_list',
+			'preview_size' => array( 100, 100 ),
+			'text'         =>
+				array(
+					'add_upload_files_text' => __( 'Add or Upload Images/Videos' ), // default: "Add or Upload Files"
+					'file_text'             => __( 'Image/Video:' ), // default: "File:"
+				),
+			'repeatable'   => false
+		)
+	);
+
+	// Downloadable Plains
+	$cmb_property->add_field( array(
+		'name'    => 'Plains (compressed file)',
+		'desc'    => 'Upload a .zip file',
+		'id'      => $prefix . 'pzip',
+		'type'    => 'file',
+		// Optional:
+		'options' => array(
+			'url' => false, // Hide the text input for the url
+		),
+		'text'    => array(
+			'add_upload_file_text' => 'Add File' // Change upload button text. Default: "Add or Upload File"
+		)
+	) );
+
+	$cmb_property->add_field(
+		array(
+			'name'       => __( 'Plains button text' ),
+			'id'         => $prefix . 'pbtn_es_id',
+			'type'       => 'text',
+			'repeatable' => false,
+			'desc'       => 'Example: Descargar Planos / Download plains'
+		)
+	);
+
+	// Quality Memory
+	$cmb_property->add_field(
+		array(
+			'name'    => 'Quality Memory',
+			'id'      => $prefix . 'quality',
+			'type'    => 'wysiwyg',
+			'options' => array(),
+		)
+	);
+
+	// Downloadable Memory
+	$cmb_property->add_field( array(
+		'name'    => __( 'Downloadable Memory' ),
+		'desc'    => __( 'Upload file' ),
+		'id'      => $prefix . 'memofiles',
+		'type'    => 'file',
+		// Optional:
+		'options' => array(
+			'url' => false, // Hide the text input for the url
+		),
+		'text'    => array(
+			'add_upload_file_text' => 'Add File' // Change upload button text. Default: "Add or Upload File"
+		)
+	) );
+
+	$cmb_property->add_field(
+		array(
+			'name'       => __( 'Memory button text' ),
+			'id'         => $prefix . 'mbtn_es_id',
+			'type'       => 'text',
+			'repeatable' => false,
+			'desc'       => 'Example: Descargar Memoria / Download Mememory'
+		)
+	);
+
+	// Latitude
+	$cmb_property->add_field(
+		array(
+			'name'       => __( 'Latitude' ),
+			'id'         => $prefix . 'lat',
+			'type'       => 'text',
+			'repeatable' => false,
+			'desc'       => 'Example: 36.0954894'
+		)
+	);
+
+	// Longitude
+	$cmb_property->add_field(
+		array(
+			'name'       => __( 'Longitude' ),
+			'id'         => $prefix . 'lng',
+			'type'       => 'text',
+			'repeatable' => false,
+			'desc'       => 'Example: -115.1762154'
+		)
+	);
+
+	// Nearby Places
+	/*$cmb_property->add_field( array(
+		'name'           => 'List of Nearby Places',
+		'desc'           => 'Select one or more places',
+		'id'             => 'wiki_test_taxonomy_multicheck',
+		'taxonomy'       => 'nearby_places',
+		'type'           => 'taxonomy_multicheck_inline',
+		'text'           => array(
+			'no_terms_text' => 'Sorry, no terms could be found.'
+		),
+		'remove_default' => 'true' // Removes the default metabox provided by WP core. Pending release as of Aug-10-16
+	) );*/
+
+	// Brochure
+	$cmb_property->add_field( array(
+		'name'         => 'Brochure',
+		'desc'         => 'Upload a PDF file',
+		'id'           => $prefix . 'brochure',
+		'type'         => 'file',
+		// Optional:
+		'options'      => array(
+			'url' => false, // Hide the text input for the url
+		),
+		'text'         => array(
+			'add_upload_file_text' => 'Add File' // Change upload button text. Default: "Add or Upload File"
+		),
+		// query_args are passed to wp.media's library query.
+		'query_args'   => array(
+			'type' => 'application/pdf', // Make library only display PDFs.
+		),
+		'preview_size' => 'large', // Image size to use when previewing in the admin.
+	) );
+
+	$cmb_property->add_field(
+		array(
+			'name'       => __( 'Brochure button text' ),
+			'id'         => $prefix . 'brochure_es_id',
+			'type'       => 'text',
+			'repeatable' => false,
+			'desc'       => 'Example: Descargar PDF / Download PDF'
+		)
+	);
+
 }
---
+++ wp-content/themes/hr19/partials/presale.php
@@ -0,0 +1,285 @@
+<?php
+$intdetails       = get_post_meta( get_the_ID(), '_pr_intdetails', true );
+$intdet           = explode( '<!--more-->', $intdetails );
+$intimages        = get_post_meta( get_the_ID(), '_pr_intimages', true );
+$background_image = wp_get_attachment_url( get_post_meta( get_the_ID(), '_pr_images_id', true ) );
+$amenities        = get_post_meta( get_the_ID(), '_pr_amen', true );
+$location         = get_post_meta( get_the_ID(), '_pr_price', true );
+$amenimages       = get_post_meta( get_the_ID(), '_pr_amengallery', true );
+$plainsimages     = get_post_meta( get_the_ID(), '_pr_plainsgallery', true );
+$quality          = get_post_meta( get_the_ID(), '_pr_quality', true );
+$q                = explode( '<!--more-->', $quality );
+$lng              = get_post_meta( get_the_ID(), '_pr_lng', true );
+$lat              = get_post_meta( get_the_ID(), '_pr_lat', true );
+$brochure         = wp_get_attachment_url( get_post_meta( get_the_ID(), '_pr_brochure_id', true ) );
+$bbtn_es          = get_post_meta( get_the_ID(), '_pr_brochure_es_id', true );
+$pzip             = wp_get_attachment_url( get_post_meta( get_the_ID(), '_pr_pzip_id', true ) );
+$pbtn_es          = get_post_meta( get_the_ID(), '_pr_pbtn_es_id', true );
+$terms            = get_the_terms( get_the_ID(), 'nearby_places' );
+$memofiles        = wp_get_attachment_url( get_post_meta( get_the_ID(), '_pr_memofiles_id', true ) );
+$mbtn_es          = get_post_meta( get_the_ID(), '_pr_mbtn_es_id', true );
+$placeholder      = get_template_directory_uri() . '/assets/no-photo.jpg';
+?>
+
+<section class="prop-header text-center"
+         style="background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 0%,rgba(0, 0, 0, 0.5) 100%), url(<?php echo( ! empty( $background_image ) ? $background_image : $placeholder ) ?>)">
+    <h1><?php the_title(); ?></h1>
+	<?php if ( ! empty( $location ) ) { ?><h2><?php echo $location; ?></h2><?php } ?>
+	<?php if ( ! empty( $brochure ) ) { ?>
+        <a class="download-bro" href="<?php echo $brochure; ?>" download><?php echo $bbtn_es ?></a>
+	<?php } ?>
+</section>
+
+<section id="property-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+				<?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 detail-content">
+				<?php if ( ! empty( $intdetails ) or ! empty( $intimages ) ) { ?>
+                    <div class="detail-tlt"><?php echo( $lang == "es_ES" ? 'Detalles del Interior' : 'Interior details' ) ?></div><?php } ?>
+                <p><?php if ( ! empty( $intdetails ) ) {
+						echo $intdetails;
+					} ?></p>
+				<?php if ( ! empty( $intimages ) ) { ?>
+                    <div class="row gallery-images">
+						<?php $counter = 0;
+						foreach ( (array) $intimages as $attachment_id => $attachment_url ) { ?>
+                            <div class="col-sm-4 col-md-4">
+                                <a href="#" class="amenimg gallery-detalles" data-number="<?php echo $counter; ?>"
+                                   data-toggle="modal" data-target="#myModalDetails"
+                                   style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'full' ); ?>')"></a>
+                            </div>
+							<?php $counter ++;
+						} ?>
+                    </div>
+				<?php } ?>
+                <div id="myModalDetails" class="modal fade modal-detail" role="dialog">
+                    <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times"></i></button>
+                    <div class="modal-dialog modal-lg">
+                        <div id="interior-gallery" class="modal-content">
+                            <div class="modal-body">
+                                <div class="swiper-container gallery-top-details swiper-detail">
+                                    <div class="swiper-wrapper">
+										<?php foreach ( (array) $intimages as $attachment_id => $attachment_url ) { ?>
+                                            <div class="swiper-slide"
+                                                 style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'full' ); ?>')"></div>
+										<?php } ?>
+                                    </div>
+                                    <div class="swiper-button-next swiper-button-white"></div>
+                                    <div class="swiper-button-prev swiper-button-white"></div>
+                                </div>
+                                <div class="swiper-container gallery-thumbs-details swiper-detail-thumbs">
+                                    <div class="swiper-wrapper">
+										<?php foreach ( (array) $intimages as $attachment_id => $attachment_url ) { ?>
+                                            <div class="swiper-slide"
+                                                 style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'thumb' ); ?>')"></div>
+										<?php } ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+				<?php if ( ! empty( $amenities ) or ! empty( $amenimages ) ) { ?>
+                    <div class="detail-tlt"><?php echo( $lang == "es_ES" ? 'Comodidades' : 'Amenities' ) ?></div><?php } ?>
+                <p><?php if ( ! empty( $amenities ) ) {
+						echo $amenities;
+					} ?></p>
+				<?php if ( ! empty( $amenimages ) ) { ?>
+                    <div class="row gallery-images">
+						<?php $counter = 0;
+						foreach ( (array) $amenimages as $attachment_id => $attachment_url ) { ?>
+                            <div class="col-sm-4 col-md-4">
+                                <a href="#" class="amenimg gallery-comodidades"
+                                   data-number="<?php echo $counter; ?>" data-toggle="modal" data-target="#myModal"
+                                   style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'full' ); ?>')"></a>
+                            </div>
+							<?php $counter ++;
+						} ?>
+                    </div>
+				<?php } ?>
+                <div id="myModal" class="modal fade modal-detail" role="dialog">
+                    <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times"></i></button>
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div id="amenities-gallery" class="modal-body">
+                                <div class="swiper-container gallery-top swiper-detail">
+                                    <div class="swiper-wrapper">
+										<?php foreach ( (array) $amenimages as $attachment_id => $attachment_url ) { ?>
+                                            <div class="swiper-slide"
+                                                 style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'full' ); ?>')"></div>
+										<?php } ?>
+                                    </div>
+                                    <div class="swiper-button-next swiper-button-white"></div>
+                                    <div class="swiper-button-prev swiper-button-white"></div>
+                                </div>
+                                <div class="swiper-container gallery-thumbs swiper-detail-thumbs">
+                                    <div class="swiper-wrapper">
+										<?php foreach ( (array) $amenimages as $attachment_id => $attachment_url ) { ?>
+                                            <div class="swiper-slide"
+                                                 style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'full' ); ?>')"></div>
+										<?php } ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	<?php if ( ! empty( $plainsimages ) ) { ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="detail-tlt"><?php echo( $lang == "es_ES" ? 'Planos' : 'Floorplans' ) ?></div>
+                    <div id="gallery-top-blueprint" class="swiper-container gallery-top-blueprint swiper-detail">
+                        <div class="swiper-wrapper">
+							<?php foreach ( (array) $plainsimages as $attachment_id => $attachment_url ) { ?>
+								<?php $filetype = wp_check_filetype( $attachment_url );
+								$name           = basename( get_attached_file( $attachment_id ) ); ?>
+								<?php if ( $filetype['ext'] == 'mp4' ) { ?>
+                                    <div class="swiper-slide ">
+                                        <div class="bp-title">
+                                            <span>Video: <?php echo $attachment_title = get_the_title( $attachment_id ); ?></span>
+                                        </div>
+                                        <video id="<?php echo substr( $name, 0, strrpos( $name, '.' ) ); ?>"
+                                               width="100%" controls>
+                                            <source src="<?php echo wp_get_attachment_url( $attachment_id, 'full' ); ?>"
+                                                    type="video/mp4">
+                                            Your browser does not support HTML5 video.
+                                        </video>
+                                    </div>
+								<?php } else { ?>
+                                    <div class="swiper-slide swiper-slide-bp"
+                                         style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'full' ); ?>')">
+                                        <div class="bp-title">
+                                            <span><?php echo $attachment_title = get_the_title( $attachment_id ); ?></span>
+                                        </div>
+                                    </div>
+								<?php } ?>
+							<?php } ?>
+                        </div>
+                    </div>
+                    <div class="al-div-border"></div>
+                    <div id="gallery-thumbs-blueprint"
+                         class="swiper-container gallery-thumbs-blueprint swiper-detail-thumbs border-thumb">
+                        <div class="swiper-wrapper">
+							<?php foreach ( (array) $plainsimages as $attachment_id => $attachment_url ) { ?>
+								<?php $filetype = wp_check_filetype( $attachment_url );
+								$name           = basename( get_attached_file( $attachment_id ) ); ?>
+								<?php if ( $filetype['ext'] == 'mp4' ) { ?>
+                                    <div class="swiper-slide blueprint-img play-video"
+                                         data-id="<?php echo substr( $name, 0, strrpos( $name, '.' ) ); ?>"><i
+                                                class="fa fa-play-circle"></i></div>
+								<?php } else { ?>
+                                    <div class="swiper-slide blueprint-img"
+                                         style="background: url('<?php echo wp_get_attachment_url( $attachment_id, 'thumb' ); ?>')"></div>
+								<?php } ?>
+							<?php } ?>
+                        </div>
+                    </div>
+                    <div class="swiper-button-next swiper-button-white"></div>
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                    <div class="text-center">
+						<?php if ( ! empty( $pzip ) ) { ?>
+                            <a class="btn-planos" href="<?php echo $pzip; ?>" download><?php echo $pbtn_es ?></a>
+						<?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+	<?php } ?>
+
+	<?php if ( ! empty( $quality ) ) { ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="detail-tlt"><?php echo( $lang == "es_ES" ? 'Memoria de Calidad' : 'Memories' ) ?></div>
+                    <p><?php echo $q[0]; ?></p>
+                    <div class="row memory-items">
+                        <div class="col-sm-4 col-md-4 text-center">
+							<?php echo $q[1]; ?>
+                        </div>
+                        <div class="col-sm-4 col-md-4 text-center">
+							<?php echo $q[2]; ?>
+                        </div>
+                        <div class="col-sm-4 col-md-4 text-center">
+							<?php echo $q[3]; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 text-center">
+					<?php if ( ! empty( $memofiles ) ) { ?>
+                        <a class="btn-planos" href="<?php echo $memofiles; ?>" download><?php echo $mbtn_es ?></a>
+					<?php } ?>
+                </div>
+            </div>
+        </div>
+	<?php } ?>
+
+	<?php if ( ! empty( $lat ) && ! empty( $lng ) ) { ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="detail-tlt"><?php echo( $lang == "es_ES" ? 'Ubicaci&oacute;n' : 'Location' ) ?></div>
+                    <div id="map"></div>
+                    <div id="save-widget">
+                        <strong><?php the_title() ?></strong>
+                        <p>
+                            <a href="https://www.google.com/maps/place/<?php echo $lat ?>,<?php echo $lng ?>"
+                               target="_blank">
+								<?php echo( $lang == "es_ES" ? 'Ver en Google Maps' : 'See in Google Maps' ) ?>
+                            </a>
+                        </p>
+                    </div>
+                    <script async defer
+                            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH6eKdzSWQ0bic1EiZCzkbhqMqEzYd3eQ&callback=initMap">
+                    </script>
+                    <script>
+                        function initMap() {
+                            var uluru = {lat: <?php echo $lat ?>, lng: <?php echo $lng ?>};
+                            var map = new google.maps.Map(document.getElementById('map'), {
+                                zoom: 10,
+                                center: uluru
+                            });
+                            var widgetDiv = document.getElementById('save-widget');
+                            map.controls[google.maps.ControlPosition.TOP_LEFT].push(widgetDiv);
+
+                            // Append a Save Control to the existing save-widget div.
+                            var saveWidget = new google.maps.SaveWidget(widgetDiv, {
+                                attribution: {
+                                    source: 'Google Maps JavaScript API',
+                                    webUrl: 'https://developers.google.com/maps/'
+                                }
+                            });
+                            var alamarker = '<?php echo get_template_directory_uri(); ?>/assets/marker.png';
+                            var marker = new google.maps.Marker({
+                                position: uluru,
+                                map: map,
+                                icon: alamarker,
+                                animation: google.maps.Animation.BOUNCE
+                            });
+                        }
+                    </script>
+                </div>
+            </div>
+        </div>
+	<?php } ?>
+
+</section>
